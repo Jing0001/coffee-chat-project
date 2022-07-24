c16faa3fe15efc2fe3177303ac715a0a98bf5619
Jing Feng <flora@Jings-MacBook-Pro-2.local>
2022-07-24 13:46:06 -0700

    initial commit

---
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MessageCell.swift
+//  Coffee Chat iOS13
+//
+//  Created by Jing Feng.
+//
+
+import UIKit
+import FirebaseCore
+import FirebaseAuth
+
+class RegisterViewController: UIViewController {
+
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
+    @IBAction func registerPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text, let password = passwordTextfield.text{
+        
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            if let e = error {
+                print(e.localizedDescription)
+            } else {
+                self.performSegue(withIdentifier: K.registerSegue, sender: self)
+            }
+            }
+        }
+    }
+}
---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  Coffee Chat iOS13
+//
+//  Created by Jing Feng.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
